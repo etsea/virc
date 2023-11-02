69ed91c07028c40f6344fc44d6f166db68cc016a
Jeff Vos <jeff.vos@icloud.com>
2023-11-01 22:39:10 -0400

    refactor(ui) added command line arguments
    - Added use of command line arguments using the `cli` module
    - Added command `start` to initiate an IRC connection
    - Added flag `-h` to set an IRC host
    - Added flag `-p` to set an IRC host port
    - Added flag `-n` to set an IRC nickname
    - Added flag `-pass` to set an IRC password

--- src/main.v
+++ src/main.v
@@ -3,16 +3,69 @@ module main
 import irc
 import helper
 
-fn main() {
-	mut host := 'irc.libera.chat'
-	mut port := '6667'
-	mut nickname := ''
-	mut password := ''
+import os
+import cli
 
-	if nickname == '' { nickname = helper.random_nickname() }
+fn main() {
+	mut app := cli.Command{
+		name: 'virc'
+		description: 'A simple IRC client written in V'
+		version: '0.0.1'
+		disable_man: true
+	}
+	mut start := cli.Command{
+		name: 'start'
+		description: 'Start an IRC connection'
+		execute: irc_connect
+	}
+	mut nick := cli.Flag{
+		flag: .string
+		name: 'nick'
+		abbrev: 'n'
+		description: 'Set an IRC nickname.'
+		default_value: [helper.random_nickname()]
+		global: true
+	}
+	mut host := cli.Flag{
+		flag: .string
+		name: 'host'
+		abbrev: 'h'
+		description: 'Set an IRC server'
+		default_value: ['irc.libera.chat']
+		global: true
+	}
+	mut port := cli.Flag{
+		flag: .string
+		name: 'port'
+		abbrev: 'p'
+		description: 'Set an IRC port number'
+		default_value: ['6667']
+		global: true
+	}
+	mut password := cli.Flag{
+		flag: .string
+		name: 'password'
+		abbrev: 'pass'
+		description: 'Set an IRC password'
+		default_value: ['']
+		global: true
+	}
+
+	app.add_command(start)
+	app.add_flags([nick, host, port, password])
+	app.setup()
+	app.parse(os.args)
+}
+
+fn irc_connect(cmd cli.Command) ! {
+	host := cmd.flags.get_string('host')!
+	port := cmd.flags.get_string('port')!
+	nick := cmd.flags.get_string('nick')!
+	password := cmd.flags.get_string('password')!
+
 
 	mut instance := irc.Interface{}
-	instance.init(host, port, password, nickname) or {
+	instance.init(host, port, password, nick) or {
 		eprintln('Error initializing server connection: ${err}')
 	}
 
